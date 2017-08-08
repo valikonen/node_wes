0e2004643d4eb6bcbf9ec2dfbe5efa670db403f6
Valentin Carnaru <valikonen@gmail.com>
2017-08-08 09:54:44 +0300

    tt

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,3 +4,11 @@ exports.homePage = (req, res) => {
     console.log('====================================');
     res.render("index");
 }
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add store' });
+}
+
+exports.createStore = ( req, res ) => {
+    res.json( req.body );
+}
