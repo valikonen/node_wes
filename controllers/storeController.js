28ad732c5244728e1ace50c506b170c7efe98fdc
Dragos Apostol <dragos.apostol1@ro.ibm.com>
2017-08-11 17:12:14 +0300

    Add recipes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,9 +10,17 @@ exports.addStore = (req, res) => {
 }
 
 exports.createStore = async ( req, res ) => {
-    const store = new Store(req.body);
-    console.log("req.body: ", req.body);
-    await store.save();
-    res.redirect("/");
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}`);
+    res.redirect(`/store/${store.slug}`);
 }
 
+exports.getStores = async (req, res) => {
+
+    // 1. Query to the DB for the list of stores
+    const stores = await Store.find();
+    console.log(stores); 
+
+    res.render('stores', { title: 'Store', stores: stores });
+
+}
